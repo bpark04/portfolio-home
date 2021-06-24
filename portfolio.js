51fbe12eb66dcf96a0df105ea264de8d27942c11
bpark04 <80388087+bpark04@users.noreply.github.com>
2021-06-23 21:50:03 -0700

    Add files via upload

--- portfolio.js
+++ portfolio.js
@@ -1,4 +1,3 @@
-
 /* HEADER */
 
 window.onload = function() {scrollTop()};
@@ -77,7 +76,46 @@ function showImg(n) {
 document.getElementById('imagePrev').addEventListener('click', showNext.bind(null,-1));
 document.getElementById('imageNext').addEventListener('click', showNext.bind(null,1));
 
-document.getElementById('firstDot').addEventListener('click', showCurrent.bind(null,1));
-document.getElementById('secondDot').addEventListener('click', showCurrent.bind(null,2));
-document.getElementById('thirdDot').addEventListener('click', showCurrent.bind(null,3));
-document.getElementById('forthDot').addEventListener('click', showCurrent.bind(null,4));
+
+
+filterSelection('all');
+
+function filterSelection(id) {
+  var x, i;
+
+  x = document.getElementsByClassName('listItem');
+  for(i=0;i<x.length;i++){
+    removeClass(x[i], 'active');
+  }
+  addClass(document.getElementById(id), 'active');
+
+  x = document.getElementsByClassName('filterItem');
+  if(id == 'all') id = '';
+  for(i=0;i<x.length;i++){
+    removeClass(x[i], 'show');
+    if(x[i].className.indexOf(id) > -1)
+      addClass(x[i], 'show');
+  }
+}
+
+function addClass(element, name) {
+    if(element.className.indexOf(name) == -1) {
+      element.className += " " + name;
+    }
+}
+
+function removeClass(element, name) {
+  var arr;
+  arr = element.className.split(" ");
+
+ while(arr.indexOf(name) > -1){
+   arr.splice(arr.indexOf(name), 1);
+ }
+ /* 배열의 원소들을 연결하여 하나의 값으로 만듭니다. */
+ element.className = arr.join(" ");
+}
+
+document.getElementById('all').addEventListener('click', filterSelection.bind(null, 'all'));
+document.getElementById('uiux').addEventListener('click', filterSelection.bind(null, 'uiux'));
+document.getElementById('js').addEventListener('click', filterSelection.bind(null, 'js'));
+document.getElementById('db').addEventListener('click', filterSelection.bind(null, 'db'));
